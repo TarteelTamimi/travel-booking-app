858123c5c261507ffe64bc84fcba7d2242e3f34e
Tarteel Tamimi <tamimitarteel@gmail.com>
2024-07-08 00:58:58 +0300

    Create the login form

---
+++ src/schemas/loginFormSchema.ts
@@ -0,0 +1,12 @@
+import * as yup from "yup";
+
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+export const loginFormSchema = yup.object().shape({
+  email: yup.string().email("Please enter a valid email").required("Required"),
+  password: yup
+    .string()
+    .min(8)
+    .matches(passwordRules, { "message": "Please create a stronger password" })
+    .required("Required"),
+});
