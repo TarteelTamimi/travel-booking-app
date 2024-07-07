b8a63119f775c5f2f5ba196c6e1c64b83b2d3891
Tarteel Tamimi <tamimitarteel@gmail.com>
2024-07-07 15:02:57 +0300

    Test the APIs

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react"
+import { api } from "../services/api"
 
 const HomePage = () => {
+  useEffect(() => {
+    api.get('/api/cities')
+    .then(res => res.data)
+    .then(console.log)
+    
+  }, [])
   return (
     <div>
       <h1>Home Page</h1>      
