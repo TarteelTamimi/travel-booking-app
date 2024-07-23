a395085a96e3dc5f12d047446a6334a44dec799c
Tarteel Tamimi <tamimitarteel@gmail.com>
2024-07-23 22:03:41 +0300

    Design the 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,20 @@
+import { FaArrowLeft } from "react-icons/fa";
+import notFound from "../assets/undraw_page_not_found_re_e9o6.svg";
+
 const NotFoundPage = () => {
+
   return (
-    <div>
-      <h1>404</h1>
+    <div className="absolute top-40 left-[500px] flex">
+      <div className="flex flex-col">
+        <div className="flex flex-col items-center">
+          <img src={notFound} alt="not found" width={500} height={500} />
+          <h3 className="text-2xl font-bold p-10">Page Not Found :(</h3>
+        </div>
+        <a href="/home" className="flex hover:cursor-pointer hover:text-blue-800 hover:underline">
+          <FaArrowLeft className="mr-2 mt-[5px]" />
+          Back to home page
+        </a>
+      </div>
     </div>
   )
 }
