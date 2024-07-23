5f4416903a1436848099122946267fce84661304
Tarteel Tamimi <tamimitarteel@gmail.com>
2024-07-23 23:44:33 +0300

    Complete the hotels page

--- src/components/Hotels/Hotels.tsx
+++ src/components/Hotels/Hotels.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FiAlertTriangle } from "react-icons/fi";
 import { useFetchHotels } from "../../hooks/useFetchHotels.hook";
 import { HotelModel } from "../../models/Hotel";
@@ -5,6 +6,18 @@ import HotelCard from "./HotelCard";
 
 const Hotels = () => {
   const { hotels, loading, error } = useFetchHotels();
+  const [isSorted, setIsSorted] = useState(false);
+  const [sortedHotels, setSortedHotels] = useState<HotelModel[]>([]);
+
+  const handleSortToggle = () => {
+    if (isSorted) {
+      setSortedHotels(hotels);
+    } else {
+      const sorted = [...hotels].sort((a, b) => b.starRating - a.starRating);
+      setSortedHotels(sorted);
+    }
+    setIsSorted(!isSorted);
+  };
 
   if (loading) {
     return <span className="hotel-page-loader"></span>
@@ -19,14 +32,28 @@ const Hotels = () => {
     )
   }
 
+  const displayedHotels = isSorted ? sortedHotels : hotels;
+
   return (
-    <div className="ml-20">
-      {hotels.map((hotel: HotelModel) => (
-        <div key={hotel.id}>
-          <HotelCard {...hotel} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="absolute top-[8.5rem] right-10">
+        <button
+          id="dropdownRadioButton"
+          onClick={handleSortToggle}
+          className="inline-flex p-2 items-center text-gray-600 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-lg text-sm px-3 py-1.5"
+          type="button"
+        >
+          {isSorted ? "Sorted By:  Star Rating" : "Sorted By:  Default"}
+        </button>
+      </div>
+      <div className="ml-20 grid grid-cols-1 md:grid-cols-2 gap-4">
+        {displayedHotels.map((hotel: HotelModel) => (
+          <div key={hotel.id}>
+            <HotelCard {...hotel} />
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
--- src/pages/HotelsPage.tsx
+++ src/pages/HotelsPage.tsx
@@ -1,6 +1,7 @@
 import Hotels from "../components/Hotels/Hotels";
 
 const HotelsPage = () => {
+
   return (
     <div className="flex flex-col bg-gray-200">
       <h2 className="text-5xl font-bold p-10">Hotels</h2>
