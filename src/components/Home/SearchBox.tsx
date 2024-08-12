8e99b8f85f1537a1994f1fcc7698798895ed64f2
Tarteel Tamimi <tamimitarteel@gmail.com>
2024-08-12 21:59:18 +0300

    Re-design the search box

--- src/components/Home/SearchBox.tsx
+++ src/components/Home/SearchBox.tsx
@@ -70,67 +70,71 @@ const SearchBox = () => {
   }
 
   return (
-    <div className="bg-blue-700 p-4 rounded-lg flex flex-wrap gap-4 items-center justify-evenly w-full">
-      <div className="flex flex-col flex-1 max-w-[180px]">
-        <label htmlFor="city" className="text-white mb-1">Location:</label>
-        <div className="relative flex items-center border-2 border-white rounded-lg">
-          <IoLocationOutline className="text-white text-3xl m-1" />
-          <input
-            type="text"
-            id="city"
-            value={city}
-            onChange={handleCityChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            className="text-white bg-blue-700 focus:outline-none font-medium rounded-lg text-lg py-2 pl-8 placeholder-gray-400 w-full"
-            placeholder="City"
-          />
-          {showList && (
-            <div className="absolute top-full left-0 mt-1 bg-white divide-y divide-gray-100 rounded-lg shadow w-full max-h-60 overflow-auto z-10">
-              <ul className="py-2 text-sm text-gray-700">
-                {filteredCities.length
-                  ? filteredCities.map((city: CityModel) => (
-                    <li
-                      key={city.id}
-                      onClick={() => handleCityClick(city)}
-                      className="block px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                    >
-                      {city.name}
-                    </li>
-                  ))
-                  : <p className="px-4 py-2 text-gray-500">No result</p>
-                }
-              </ul>
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="flex flex-col flex-1 max-w-[180px]">
-        <label htmlFor="startDate" className="text-white mb-1">From:</label>
-        <div className="relative flex items-center border-2 border-white rounded-lg">
-          <CgCalendarDates className="text-white text-3xl m-1" />
-          <DatePicker
-            selected={startDate}
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onChange={(date: any) => setStartDate(date)}
-            className="text-white bg-blue-700 focus:outline-none font-medium rounded-lg text-lg py-2 pl-8 placeholder-gray-400 w-full"
-          />
-        </div>
-      </div>
-      <div className="flex flex-col flex-1 max-w-[180px]">
-        <label htmlFor="endDate" className="text-white mb-1">To:</label>
-        <div className="relative flex items-center border-2 border-white rounded-lg">
-          <CgCalendarDates className="text-white text-3xl m-1" />
-          <DatePicker
-            selected={endDate}
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onChange={(date: any) => setEndDate(date)}
-            className="text-white bg-blue-700 focus:outline-none font-medium rounded-lg text-lg py-2 pl-8 placeholder-gray-400 w-full"
-          />
-        </div>
-      </div>
-      <div className="flex flex-col">
+    <div className="bg-blue-700 p-4 rounded-lg flex flex-wrap gap-20 items-center justify-center w-11/12 m-auto max-[1050px]:w-full max-[556px]:gap-10">
+      <div className="flex flex-col gap-5">
         <div className="flex flex-col flex-1 max-w-[180px]">
+          <label htmlFor="city" className="text-white mb-1">Location:</label>
+          <div className="relative flex items-center border-2 border-white rounded-lg">
+            <IoLocationOutline className="text-white text-4xl m-1" />
+            <input
+              type="text"
+              id="city"
+              value={city}
+              onChange={handleCityChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              className="text-white bg-blue-700 focus:outline-none font-medium rounded-lg text-lg py-2 pl-8 placeholder-gray-400 w-full"
+              placeholder="City"
+            />
+            {showList && (
+              <div className="absolute top-full left-0 mt-1 bg-white divide-y divide-gray-100 rounded-lg shadow w-full max-h-60 overflow-auto z-10">
+                <ul className="py-2 text-sm text-gray-700">
+                  {filteredCities.length
+                    ? filteredCities.map((city: CityModel) => (
+                      <li
+                        key={city.id}
+                        onClick={() => handleCityClick(city)}
+                        className="block px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                      >
+                        {city.name}
+                      </li>
+                    ))
+                    : <p className="px-4 py-2 text-gray-500">No result</p>
+                  }
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="flex gap-6 max-[1050px]:flex-col">
+          <div className="flex flex-col flex-1 max-w-[180px]">
+            <label htmlFor="startDate" className="text-white mb-1">From:</label>
+            <div className="relative flex items-center border-2 border-white rounded-lg">
+              <CgCalendarDates className="text-white text-4xl m-1" />
+              <DatePicker
+                selected={startDate}
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                onChange={(date: any) => setStartDate(date)}
+                className="text-white bg-blue-700 focus:outline-none font-medium rounded-lg text-lg py-2 pl-8 placeholder-gray-400 w-full"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col flex-1 max-w-[180px]">
+            <label htmlFor="endDate" className="text-white mb-1">To:</label>
+            <div className="relative flex items-center border-2 border-white rounded-lg">
+              <CgCalendarDates className="text-white text-4xl m-1" />
+              <DatePicker
+                selected={endDate}
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                onChange={(date: any) => setEndDate(date)}
+                className="text-white bg-blue-700 focus:outline-none font-medium rounded-lg text-lg py-2 pl-8 placeholder-gray-400 w-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col flex-1 max-w-[200px]">
           <label htmlFor="numberOfAdults" className="text-white mb-1">Number Of Adults:</label>
           <input
             type="number"
@@ -166,7 +170,7 @@ const SearchBox = () => {
       </div>
       <button
         onClick={handleSearch}
-        className="border-2 border-white bg-white text-blue-700 text-center rounded-lg text-lg font-bold py-2 px-4 hover:bg-gray-200 flex-shrink-0"
+        className="border-2 border-white bg-white text-blue-700 text-center rounded-lg text-lg min-w-[16rem] font-bold py-2 px-4 hover:bg-gray-200 flex-shrink-0"
       >
         Search
       </button>
